c9b087ec13479d42c19e81397efa94cd96ce3b02
Polina Silina <gomik3112002@gmail.com>
2025-10-29 21:53:59 +0300

    add new lines in "Roman_1" add code in "Polina_2"

--- Roman_1.py
+++ Roman_1.py
@@ -3,18 +3,20 @@ class Human:
         self,
         name: str,
         age: int,
-        job: str
+        job: str,
+        experience: int
     ) -> None:
         self.name = name
         self.age = age
         self.job = job
+        self.experience=experience
 
     def greeting(self) -> str:
-        print(f"Hi! My name is {self.name}, I'm {self.age} years old. I work as {self.job} for OpenAI.")
+        print(f"Hi! My name is {self.name}, I'm {self.age} years old. I work as {self.job} for OpenAI {self.experience} years of experience.")
         print(f"HAHAHHA Staryi Kon' borozdy ne isportit!")
         print(f"if we knew what it is...")
 
 
 if __name__ == "__main__":
-    human = Human("Roman", 150, "data engineer")
+    human = Human("Roman", 150, "data engineer", experience=55)
     human.greeting()
